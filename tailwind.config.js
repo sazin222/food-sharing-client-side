34090d55fece714a06ce039920b7a9c4755bf4e8
sazin222 <mahadiislamsazin.com>
2023-11-04 21:35:48 +0600

    react routerdom and tailwind and prelindcomponents has been installed and setup

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    'node_modules/preline/dist/*.js',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('preline/plugin'),
+],
+}
+
