2f5f345a672d28ae230d82f733878c2565503e3d
sazin222 <mahadiislamsazin.com>
2023-11-05 13:32:18 +0600

    login and register page has been added

--- src/Pages/Home/Navber.jsx
+++ src/Pages/Home/Navber.jsx
@@ -10,11 +10,11 @@ const Navber = () => {
     isPending ? "pending" : isActive ? "border-b-4 border-green-600 font-bold " : ""} to={"/"} >Home</NavLink>  
       <NavLink to={"/availableFoods"} className={({ isActive, isPending }) =>
     isPending ? "pending" : isActive ? "border-b-4 border-green-600 font-bold " : ""}>Available Foods</NavLink>
-     <NavLink to={"/login"} className={({ isActive, isPending }) =>
-    isPending ? "pending" : isActive ? "border-b-4 border-green-600 font-bold " : ""} >Add Food</NavLink>
      <NavLink to={"/addFood"} className={({ isActive, isPending }) =>
-    isPending ? "pending" : isActive ? "border-b-4 border-green-600 font-bold " : ""} >Manage My Foods</NavLink>
+    isPending ? "pending" : isActive ? "border-b-4 border-green-600 font-bold " : ""} >Add Food</NavLink>
      <NavLink to={"/manageFood"} className={({ isActive, isPending }) =>
+    isPending ? "pending" : isActive ? "border-b-4 border-green-600 font-bold " : ""} >Manage My Foods</NavLink>
+     <NavLink to={"/foodRequest"} className={({ isActive, isPending }) =>
     isPending ? "pending" : isActive ? "border-b-4 border-green-600 font-bold " : ""} > My Food Request</NavLink>
      
     </>
@@ -73,22 +73,13 @@ const Navber = () => {
            {Navlinks}
             
            <div className="flex items-center gap-x-2 sm:ml-24">
-              <a
-                className="flex items-center gap-x-2 font-medium text-gray-500 hover:text-blue-600 dark:text-gray-400 dark:hover:text-blue-500"
-                href="#"
-              >
-                <svg
-                  className="w-4 h-4"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="16"
-                  height="16"
-                  fill="currentColor"
-                  viewBox="0 0 16 16"
-                >
-                  <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z" />
-                </svg>
+             <NavLink to={"/login"} className={
+             ({ isActive, isPending }) =>
+             isPending ? "pending" : isActive ? "border-b-4 border-green-600 font-bold " : ""
+            }>
                 Log in
-              </a>
+              
+             </NavLink>
             </div>
           </div>
          
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -5,6 +5,8 @@ import {
 import Root from "../Layout/Root";
 import Home from "../Pages/Home/Home";
 import ErrorPage from "../Pages/ErrorPage/ErrorPage";
+import Login from "../Pages/Login/Login";
+import Register from "../Pages/Register/Register";
 
 const Routes = createBrowserRouter([
     {
@@ -15,6 +17,14 @@ const Routes = createBrowserRouter([
         {
             path:'/',
             element: <Home></Home>
+        },
+        {
+          path:'/login',
+          element:<Login></Login>
+        },
+        {
+          path:'/register',
+          element:<Register></Register>
         }
       ]
     },
